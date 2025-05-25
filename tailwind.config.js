977406c96576fd05d704ddbc5a37c90cbe80052f
Jamie Wells <jamiewells528@gmail.com>
2025-05-25 18:16:11 +0100

    boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,21 +13,10 @@ module.exports = {
     extend: {
       fontFamily: {
         //forge-insert:fonts
-        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        sans: ["<font>", ...defaultTheme.fontFamily.sans],
       },
       //forge-insert:colors
-      colors: {
-  primary: {
-    DEFAULT: "#123456",
-    light: "#1f5a95",
-    dark: "#040d16",
-  },
-  secondary: {
-    DEFAULT: "#abcdef",
-    light: "#ebf3fb",
-    dark: "#6aa6e2",
-  },
-},
+      colors: "<colors>",
     },
   },
   plugins: [
